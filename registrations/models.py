346836d04f7ff40e8931f57a79939c5e1e324949
gsvr <george@westerncapelabs.com>
2016-01-21 11:28:36 +0200

    base registration app

---
+++ registrations/models.py
@@ -0,0 +1,67 @@
+import uuid
+
+from django.contrib.postgres.fields import JSONField
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Source(models.Model):
+    """ The source from which a registation originates.
+        The User foreignkey is used to identify the source based on the
+        user's api token.
+    """
+    AUTHORITY_CHOICES = (
+        ('patient', "Patient"),
+        ('advisor', "Trusted Advisor"),
+        ('hw_limited', "Health Worker Limited"),
+        ('hw_full', "Health Worker Full")
+    )
+    name = models.CharField(max_length=100, null=False, blank=False)
+    user = models.ForeignKey(User, related_name='sources', null=False)
+    authority = models.CharField(max_length=30, null=False, blank=False,
+                                 choices=AUTHORITY_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return u"%s" % self.name
+
+
+class Registration(models.Model):
+    """ A registation submitted via Vumi or other sources.
+
+    After a registation has been created, a task will fire that
+    validates if the data provided is sufficient for the stage
+    of pregnancy.
+
+    Args:
+        stage (str): The stage of pregnancy of the mother
+        data (json): Registration info in json format
+        validated (bool): True if the registation has been
+            validated after creation
+        source (object): Auto-completed field based on the Api key
+    """
+
+    STAGE_CHOICES = (
+        ('pre_birth', "Mother is pregnant"),
+        ('post_birth', "Baby has been born"),
+        ('loss', "Baby loss")
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    stage = models.CharField(max_length=30, null=False, blank=False,
+                             choices=STAGE_CHOICES)
+    data = JSONField(null=True, blank=True)
+    validated = models.BooleanField(default=False)
+    source = models.ForeignKey(Source, related_name='registrations',
+                               null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, related_name='registrations_created',
+                                   null=True)
+    updated_by = models.ForeignKey(User, related_name='registrations_updated',
+                                   null=True)
+    user = property(lambda self: self.created_by)
+
+    def __str__(self):  # __unicode__ on Python 2
+        return str(self.id)
