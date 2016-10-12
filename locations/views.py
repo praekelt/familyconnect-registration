46cb493b0e7211a8c2c86a77c92c13a42cdd6021
Rudi Giesler <rudi@praekelt.com>
2016-10-12 17:15:23 +0200

    Add view for searching through Parishes

--- locations/views.py
+++ locations/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.postgres.search import TrigramSimilarity
+from rest_framework.mixins import ListModelMixin
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from .models import Parish
+from .serializers import ParishSerializer
+
+
+class ParishSearch(ListModelMixin, GenericViewSet):
+    """
+    Viewset to search through the list of parishes.
+
+    querystring parameters:
+      name - The name of the Parish to search for
+    """
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ParishSerializer
+
+    def get_queryset(self):
+        name = self.request.query_params.get('name', None)
+
+        if name is None:
+            return Parish.objects.none()
+
+        qs = Parish.objects.all()
+        qs = qs.annotate(similarity=TrigramSimilarity('name', name))
+        qs = qs.filter(similarity__gt=0.3)
+        qs = qs.order_by('-similarity')
+
+        return qs
