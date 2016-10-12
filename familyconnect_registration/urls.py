42e5e247dafc25d9b907ac04cd9fc19a92591980
Rudi Giesler <rudi@praekelt.com>
2016-10-12 14:02:04 +0200

    Upgrade to Django 1.10

--- familyconnect_registration/urls.py
+++ familyconnect_registration/urls.py
@@ -1,19 +1,18 @@
 import os
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
+import rest_framework.authtoken.views
 
 admin.site.site_header = os.environ.get('REGISTRATIONS_TITLE',
                                         'FamilyConnect Registration Admin')
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^admin/',  include(admin.site.urls)),
     url(r'^api/auth/',
         include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api/token-auth/',
-        'rest_framework.authtoken.views.obtain_auth_token'),
+    url(r'^api/token-auth/', rest_framework.authtoken.views.obtain_auth_token),
     url(r'^', include('registrations.urls')),
     url(r'^', include('changes.urls')),
     url(r'^', include('uniqueids.urls')),
-)
+]
